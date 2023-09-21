4289db7169df828f35a7fa98f63e9f803d92da82
Jesper Ekstrøm <jesperoka@gmail.com>
2023-09-21 17:45:04 +0200

    tested sideways throwing motion

--- reboot.py
+++ reboot.py
@@ -1,3 +1,8 @@
 # Convenience rebooting just incase the robot crashes while not connected to the desk
 import panda_py
-panda_py.cli.reboot()
+
+with open('sens.txt', 'r') as file:
+    username = file.readline().strip()
+    password = file.readline().strip()
+
+panda_py.Desk("10.0.0.2", username, password).reboot()
