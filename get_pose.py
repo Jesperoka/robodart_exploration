cbee12e27e03b0a80c4c9ef2acfa77ee17bf9311
Jesper Ekstrøm <jesperoka@gmail.com>
2023-10-31 13:10:42 +0100

    added get_pose

--- get_pose.py
+++ get_pose.py
@@ -1,6 +1,13 @@
-from panda_py import Panda
+from panda_py import Panda, Desk
+
+SHOP_FLOOR_IP = "10.0.0.2"
+
+with open('sens.txt', 'r') as file:
+    username = file.readline().strip()
+    password = file.readline().strip()
+
 if __name__ == "__main__":
-#
+    Desk(SHOP_FLOOR_IP, username, password).activate_fci()
     panda = Panda(SHOP_FLOOR_IP)
     pose = panda.get_state()
     print(pose.q)
